f6d225efc71932354269fb1947c6e83ffd8917ed
Hiten Gal <hitengal@gmail.com>
2025-03-25 23:56:05 +0530

    Setup project

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <header className="bg-green-500 p-4 text-center">
+            <h1 className="text-white text-2xl m-0">Task Management</h1>
+        </header>
+    );
+};
+
+export default Header;
